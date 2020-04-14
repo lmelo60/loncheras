466f3a753bc68f8971ca0d83fb9c69c41ce844b1
lmelo60 <lmelo60@upc.edu.co>
2020-04-13 20:44:23 -0500

    routing
    roteo de paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { MainComponent } from './components/main/main.component';
+import { LoginComponent } from './components/login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: LoginComponent },
+  { path: 'inicio/registro', component: CreateAccountComponent },
+  { path: 'principal', component: MainComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CreateAccountComponent
+    CreateAccountComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
