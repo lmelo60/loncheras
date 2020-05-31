932f24c7393a4583fec4361a223732261b0b45af
lmelo60 <lmelo60@upc.edu.co>
2020-05-31 12:34:52 -0500

    Adminisrtracion cambio visual
    Se realizan algunos cambios visualez en las vista del administrador.

--- src/app/models/solicitud-response.ts
+++ src/app/models/solicitud-response.ts
@@ -2,6 +2,7 @@ import { TipoAlimento } from './tipo-alimento';
 export interface SolicitudResponse {
     HijoId: number;
     HijoNombre: string;
+    SolicitudEstado: boolean;
     SolicitudId: number;
     TipoAlimento: Array<TipoAlimento>;
     UsuarioNombre: string;
